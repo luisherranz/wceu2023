65bceec2cdefabebf033b942ecd59ab109b6d0d9
Luis Herranz <luisherranz@gmail.com>
2023-06-09 18:14:47 +0200

    step 19

--- src/blocks/quiz-progress/render.php
+++ src/blocks/quiz-progress/render.php
@@ -2,7 +2,8 @@
 $store = wp_store( array(
 	'selectors' => array(
 		'wceu2023' => array(
-			'answered' => 0
+			'answered' => 0,
+			'allAnswered' => false
 		)
 	)
 ) );
@@ -15,6 +16,12 @@ $store = wp_store( array(
 	<div>
 		<strong><?php echo __( 'Answered' ); ?></strong>: 
 		<span data-wp-text="selectors.wceu2023.answered"></span>/<?php echo count( $store['state']['wceu2023']['quizzes'] ); ?>
+	</div>
+
+	<div>
+		<button data-wp-bind--disabled="!selectors.wceu2023.allAnswered">
+			<?php echo __( 'Check your answers' ); ?>
+		</button>
 	</div>
 
 	<hr>
--- src/blocks/quiz-progress/view.js
+++ src/blocks/quiz-progress/view.js
@@ -7,6 +7,13 @@ store( {
 				Object.values( state.wceu2023.quizzes ).filter(
 					( v ) => v.current !== null
 				).length,
+			allAnswered: ( store ) => {
+				const { selectors, state } = store;
+				return (
+					selectors.wceu2023.answered( store ) ===
+					Object.keys( state.wceu2023.quizzes ).length
+				);
+			},
 		},
 	},
 } );
